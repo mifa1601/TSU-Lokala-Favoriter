e2436d4105095dc91b3a568c86ea8fe45d58f11e
Mikael Fankvist <31694144+mifa1601@users.noreply.github.com>
2018-05-11 11:15:46 +0200

    Lagt till VM

---
+++ Lokala Favoriter - app/LokalaFavoriter/LokalaFavoriter/ViewModel/HomePageVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LokalaFavoriter.ViewModel
+{
+    public class HomePageVM
+    {
+        public string LoggedInUser { get; set; }
+
+        
+    }
+}
