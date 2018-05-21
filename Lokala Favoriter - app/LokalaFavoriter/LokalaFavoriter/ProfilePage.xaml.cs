9060cee1bc0124590a9932a83ad461249a6d5ed8
Mikael Fankvist <31694144+mifa1601@users.noreply.github.com>
2018-05-21 10:14:48 +0200

    Jobbat med profile

--- Lokala Favoriter - app/LokalaFavoriter/LokalaFavoriter/ProfilePage.xaml.cs
+++ Lokala Favoriter - app/LokalaFavoriter/LokalaFavoriter/ProfilePage.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using LokalaFavoriter.Model;
+using LokalaFavoriter.Operations;
+using LokalaFavoriter.ViewModel;
+using SqlServerConnections;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +17,53 @@ namespace LokalaFavoriter
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ProfilePage : ContentPage
 	{
-		public ProfilePage ()
+        private DataTable dt;
+        private SqlServer sqls;
+        private MyData MyOperation;
+        public User MyUser = new User();
+        public Groups MyGroup = new Groups();
+
+        public ProfilePage (int user_id)
 		{
 			InitializeComponent ();
-		}
+            MyOperation = new MyData();
+            sqls = new SqlServer();
+            dt = new DataTable();
+            MyUser = MyOperation.GetUser(user_id);
+            MyGroup = MyOperation.GetGroup(user_id);
+
+            ProfilePageVM MyVm = new ProfilePageVM
+            {
+                Username = MyUser.Username,
+                Password = MyUser.Password,
+                Points = MyUser.Points,
+                Groupname = MyGroup.Groupname
+            };
+            BindingContext = MyVm;
+        }
+        #region buttons
+        void Btn_products(Object sender, System.EventArgs e)
+        {
+            var page = new ProductPage(MyUser.Id);
+            Navigation.PushAsync(page);
+        }
+        void Btn_cart(Object sender, System.EventArgs e)
+        {
+            var page = new CartPage(MyUser.Id);
+            Navigation.PushAsync(page);
+        }
+        void Btn_profile(Object sender, System.EventArgs e)
+        {
+            var page = new ProfilePage(MyUser.Id);
+            Navigation.PushAsync(page);
+        }
+        void Btn_toplist(Object sender, System.EventArgs e)
+        {
+            var page = new ToplistPage();
+            Navigation.PushAsync(page);
+        }
+        #endregion
+
         protected override void OnAppearing()
         {
             base.OnAppearing();
---
+++ Lokala Favoriter - app/LokalaFavoriter/LokalaFavoriter/ViewModel/ProfilePageVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LokalaFavoriter.ViewModel
+{
+    public class ProfilePageVM
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Points { get; set; }
+        public string Groupname { get; set; }
+    }
+}
